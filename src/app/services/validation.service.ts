f5856052e659c2bbc1fb948d71690b65f790605f
Mou97 <m.taguiouine@esi-sba.dz>
2020-04-27 18:01:05 +0100

    style home component

--- src/app/services/validation.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from "@angular/core";
-import { ValidatorFn, AbstractControl, FormGroup } from "@angular/forms";
-
-@Injectable({
-  providedIn: "root",
-})
-export class ValidationService {
-  constructor() {}
-}
